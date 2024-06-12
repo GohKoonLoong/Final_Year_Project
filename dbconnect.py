dec675d7b2ba968b192028ed09a0edba7a04583d
Goh Koon Loong <92432620+GohKoonLoong@users.noreply.github.com>
2024-06-13 01:11:11 +0800

    Update dbconnect.py

--- dbconnect.py
+++ dbconnect.py
@@ -8,7 +8,7 @@ def connect_to_database():
     # Connect to MySQL database
     conn = mysql.connector.connect(
         host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT'),
+        # port=os.getenv('DB_PORT'),
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASSWORD'),
         database=os.getenv('DB_NAME')
